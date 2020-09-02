963b55800c2f497f3d00a950569de32343c88a12
captbilard <bilardosinulu@gmail.com>
2020-09-02 23:35:55 +0100

    Authentication & Authorization(Permission) Implemented BAsic authentication in DRF Implemented authentication using JWT Created a frontend page to test session authentication

--- book/api/urls.py
+++ book/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 from . import views
@@ -18,5 +19,7 @@ urlpatterns = [
     # path('authors/<int:author_id>/', views.AuthorOne.as_view()),
     # path('books/', views.BookList.as_view() ),
     # path('books/<int:book_id>/', views.BookOne.as_view()),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
